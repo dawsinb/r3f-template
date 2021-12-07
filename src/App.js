aa4b2c69fdfbad8d9239805c99c05b285fbc4d91
dawsin <dawsinb@gmail.com>
2021-12-06 21:48:25 -0500

    change to manual webpack
    change to manual webpack + babel
    add eslint and prettier
    set up service worker

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React from "react"
-import { Canvas } from "@react-three/fiber"
-import styled from "styled-components"
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import styled from 'styled-components';
 
-const Container = styled("div")`
+const Container = styled('div')`
   width: 100%;
   height: 100%;
-`
+`;
 
 function App() {
   return (
     <Container>
-      <Canvas dpr={[2, 2]} camera={{ position: [0, 0, 100], far: 200 }}>
-        
-      </Canvas>
+      <Canvas dpr={[2, 2]} camera={{ position: [0, 0, 100], far: 200 }}></Canvas>
     </Container>
-  )
+  );
 }
 
-export { App }
+export { App };
--- src/index.js
+++ src/index.js
@@ -1,29 +1,38 @@
-import React, { Suspense } from "react"
-import ReactDOM from "react-dom"
-import { App } from "./App"
-import styled from "styled-components"
-import "./styles.css"
+/* global navigator process window document */
 
-const Container = styled("div")`
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+import styled from 'styled-components';
+import './styles.css';
+
+// register service worker
+if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/service-worker.js`);
+  });
+}
+
+const Container = styled('div')`
   width: 100vw;
   height: 100vh;
-`
+`;
 
-const LoadingContainer = styled("div")`
+const LoadingContainer = styled('div')`
   width: 100%;
   height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
   font-size: 1rem;
-`
+`;
 
 ReactDOM.render(
   <Container>
-    <Suspense fallback={<LoadingContainer children="loading..." />}>
+    <Suspense fallback={<LoadingContainer>loading...</LoadingContainer>}>
       <App />
     </Suspense>
   </Container>,
 
-  document.getElementById("root")
-)
+  document.getElementById('root')
+);
